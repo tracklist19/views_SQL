571164a5adbaac07ac606e98f568874e7b4a3cc7
tracklist19 <162835686+tracklist19@users.noreply.github.com>
2024-03-25 21:36:13 +0100

    Update v_gewaesser_be.sql

--- v_gewaesser_be.sql
+++ v_gewaesser_be.sql
@@ -24,17 +24,17 @@ row_number() over(order by ax_fliessgewaesser.ogc_fid, ax_hafenbecken.ogc_fid, a
 
 	ax_fliessgewaesser.zustand AS "ZUS", 								-- nur bis ATKIS-OK 6.0.1, siehe stehfliess_union 
 
-	ax_hafenbecken.nutzung AS "NTZ",  									-- nur bis ATKIS-OK 6.0.1, siehe stehhafen_union 
+	ax_hafenbecken.nutzung AS "NTZ",  								-- nur bis ATKIS-OK 6.0.1, siehe stehhafen_union 
 	--stehhafen_union.nutzung AS "NTZ", 								-- ab ATKIS-OK 7.1.0 
 
 	ax_stehendesgewaesser.bezeichnung AS "BEZ", 
-	--ax_stehendesgewaesser.wasserspiegelhoeheinstehendemgewaesser AS "WSG", 	-- ab ATKIS-OK 7.1.0 
+	--ax_stehendesgewaesser.wasserspiegelhoeheinstehendemgewaesser AS "WSG", 			-- ab ATKIS-OK 7.1.0 
 	
-	--stehhafenkanalwasser_union.gewaesserkennzahl AS "GWK_SKZ", 		-- ab ATKIS-OK 7.1.0 
-	stehwasserkanal_union.gewaesserkennzahl AS "GWK", 					-- bis ATKIS-OK 6.0.1, siehe stehhafenkanalwasser_union  
+	--stehhafenkanalwasser_union.gewaesserkennzahl AS "GWK_SKZ", 					-- ab ATKIS-OK 7.1.0 
+	stehwasserkanal_union.gewaesserkennzahl AS "GWK", 						-- bis ATKIS-OK 6.0.1, siehe stehhafenkanalwasser_union  
 	stehwasserkanal_union.schifffahrtskategorie AS "SFK", 
 	stehwasserkanal_union.widmung AS "WDM", 
-	--stehwasserkanal_union.regionalsprache AS "RGS",					-- ab ATKIS-OK 7.1.0 
+	--stehwasserkanal_union.regionalsprache AS "RGS",						-- ab ATKIS-OK 7.1.0 
 	--stehwasserkanal_union.zweitname AS "ZNM, 							-- ab ATKIS-OK 7.1.0 
 	
 	
@@ -57,8 +57,8 @@ FROM
 			ax_wasserlauf.identnummer, 
 			ax_wasserlauf.schifffahrtskategorie, 
 			ax_wasserlauf.widmung, 
-			ax_wasserlauf.zweitname 									-- bis ATKIS-OK 6.0.1 
-		--	ax_wasserlauf.regionalsprache, 								--  ab ATKIS-OK 7.1.0
+			ax_wasserlauf.zweitname 							-- bis ATKIS-OK 6.0.1 
+		--	ax_wasserlauf.regionalsprache, 							--  ab ATKIS-OK 7.1.0
 	FROM ax_wasserlauf 
 
 		UNION ALL 
@@ -70,8 +70,8 @@ FROM
 			ax_kanal.identnummer, 
 			ax_kanal.schifffahrtskategorie, 
 			ax_kanal.widmung, 
-			ax_kanal.zweitname 											-- bis ATKIS-OK 6.0.1 
-		--	ax_kanal.regionalsprache, 									--  ab ATKIS-OK 7.1.0
+			ax_kanal.zweitname 								-- bis ATKIS-OK 6.0.1 
+		--	ax_kanal.regionalsprache, 							--  ab ATKIS-OK 7.1.0
 	FROM ax_kanal 
 
 ) AS kanalWasserlauf_union 
@@ -83,7 +83,7 @@ RIGHT JOIN (
 		SELECT 
 		ax_fliessgewaesser.gml_id, 
 		ax_fliessgewaesser.advstandardmodell, 							-- fuer WHERE-Clause am Ende 
-		ax_fliessgewaesser.istteilvon,  								-- fuer JOIN mit kanalWasserlauf_union 
+		ax_fliessgewaesser.istteilvon,  							-- fuer JOIN mit kanalWasserlauf_union 
 		ax_fliessgewaesser.wkb_geometry
 		FROM ax_fliessgewaesser 
 	UNION ALL 
@@ -112,7 +112,7 @@ LEFT JOIN ax_stehendesgewaesser ON  stehfliesshafen_union.gml_id = ax_stehendesg
 -- gemeinsame NamensSpalte  (ab ATKIS-OK 7.1.0 : gewaesserkennzahl & seekennzahl) 
 LEFT JOIN ( 
 		SELECT 
-			ax_wasserlauf.gml_id, 										-- gml_id-Spalten fuer JOIN 
+			ax_wasserlauf.gml_id, 								-- gml_id-Spalten fuer JOIN 
 			ax_wasserlauf.name 
 			--, ax_wasserlauf.gewaesserkennzahl 						-- ab ATKIS-OK 7.1.0 
 		FROM ax_wasserlauf 
@@ -120,13 +120,13 @@ LEFT JOIN (
 		SELECT 
 			ax_kanal.gml_id, 
 			ax_kanal.name 
-			--, ax_kanal.gewaesserkennzahl 								-- ab ATKIS-OK 7.1.0 
+			--, ax_kanal.gewaesserkennzahl 							-- ab ATKIS-OK 7.1.0 
 		FROM ax_kanal 
 	UNION ALL 
 		SELECT 
 			ax_hafenbecken.gml_id, 
 			ax_hafenbecken.unverschluesselt 
-			--, ax_hafenbecken.seekennzahl 								-- ab ATKIS-OK 7.1.0 
+			--, ax_hafenbecken.seekennzahl 							-- ab ATKIS-OK 7.1.0 
 		FROM ax_hafenbecken 
 	UNION ALL 
 		SELECT 
@@ -135,8 +135,8 @@ LEFT JOIN (
 			--, ax_stehendesgewaesser.seekennzahl 						-- ab ATKIS-OK 7.1.0 
 		FROM ax_stehendesgewaesser 
 ) AS stehhafenkanalwasser_union ON stehhafenkanalwasser_union.gml_id = any(stehfliesshafen_union.istteilvon) 
-													OR stehhafenkanalwasser_union.gml_id = ax_hafenbecken.gml_id 
-													OR stehhafenkanalwasser_union.gml_id = ax_stehendesgewaesser.gml_id 
+					OR stehhafenkanalwasser_union.gml_id = ax_hafenbecken.gml_id 
+					OR stehhafenkanalwasser_union.gml_id = ax_stehendesgewaesser.gml_id 
 
 													
 -- Spalten: funktion, hydrologischesmerkmal, zustand 
@@ -145,7 +145,7 @@ LEFT JOIN (
 			ax_stehendesgewaesser.gml_id, 
 			ax_stehendesgewaesser.funktion, 
 			ax_stehendesgewaesser.hydrologischesmerkmal 
-			--, ax_stehendesgewaesser.zustand							-- ab ATKIS-OK 7.1.0
+			--, ax_stehendesgewaesser.zustand						-- ab ATKIS-OK 7.1.0
 		FROM ax_stehendesgewaesser 
 	UNION ALL 
 		SELECT 
@@ -161,20 +161,20 @@ LEFT JOIN (
 LEFT JOIN ( 
 		SELECT 
 			ax_wasserlauf.gml_id, 
-			ax_wasserlauf.gewaesserkennzahl, 							-- bis ATKIS-OK 6.0.1 
+			ax_wasserlauf.gewaesserkennzahl, 						-- bis ATKIS-OK 6.0.1 
 			ax_wasserlauf.widmung, 
 			ax_wasserlauf.schifffahrtskategorie 
-			--, ax_wasserlauf.regionalsprache							-- ab ATKIS-OK 7.1.0 
-			--, ax_wasserlauf.zweitname 								-- ab ATKIS-OK 7.1.0 
+			--, ax_wasserlauf.regionalsprache						-- ab ATKIS-OK 7.1.0 
+			--, ax_wasserlauf.zweitname 							-- ab ATKIS-OK 7.1.0 
 		FROM ax_wasserlauf 
 	UNION ALL 
 		SELECT 
 			ax_kanal.gml_id, 
-			ax_kanal.gewaesserkennzahl, 								-- bis ATKIS-OK 6.0.1 
+			ax_kanal.gewaesserkennzahl, 							-- bis ATKIS-OK 6.0.1 
 			ax_kanal.widmung, 
 			ax_kanal.schifffahrtskategorie 
-			--, ax_kanal.regionalsprache								-- ab ATKIS-OK 7.1.0 
-			--, ax_kanal.zweitname 										-- ab ATKIS-OK 7.1.0 
+			--, ax_kanal.regionalsprache							-- ab ATKIS-OK 7.1.0 
+			--, ax_kanal.zweitname 								-- ab ATKIS-OK 7.1.0 
 		FROM ax_kanal 
 	UNION ALL 
 		SELECT 
@@ -186,13 +186,13 @@ LEFT JOIN (
 			--, ax_stehendesgewaesser.zweitname 						-- ab ATKIS-OK 7.1.0 
 		FROM ax_stehendesgewaesser 
 ) AS stehwasserkanal_union ON stehwasserkanal_union.gml_id = ax_wasserlauf.gml_id 
-											OR stehwasserkanal_union.gml_id = ax_kanal.gml_id 
-											OR stehwasserkanal_union.gml_id = ax_stehendesgewaesser.gml_id
+				OR stehwasserkanal_union.gml_id = ax_kanal.gml_id 
+				OR stehwasserkanal_union.gml_id = ax_stehendesgewaesser.gml_id
 
 									
 /*
 -- Spalte: nutzung 
-LEFT JOIN ( 															-- ab ATKIS-OK 7.1.0 
+LEFT JOIN ( 												-- ab ATKIS-OK 7.1.0 
 		SELECT 
 			ax_hafenbecken.gml_id, 
 			ax_hafenbecken.nutzung 
@@ -200,13 +200,13 @@ LEFT JOIN ( 															-- ab ATKIS-OK 7.1.0
 	UNION ALL 
 		SELECT 
 			ax_stehendesgewaesser.gml_id, 
-			ax_stehendesgewaesser.nutzung 								-- ab ATKIS-OK 7.1.0 
+			ax_stehendesgewaesser.nutzung 							-- ab ATKIS-OK 7.1.0 
 		FROM ax_stehendesgewaesser 
 ) AS stehhafen_union ON stehhafen_union.gml_id = stehfliesshafen_union.gml_id 
 */
 
 
-WHERE stehfliesshafen_union.advstandardmodell @> ('{Basis-DLM}')		-- wird hier benötigt, denn es existiert advstandardmodell = DTK25
+WHERE stehfliesshafen_union.advstandardmodell @> ('{Basis-DLM}')					-- wird hier benötigt, denn es existiert advstandardmodell = DTK25
 ; 
 
 
