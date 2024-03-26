a3b4eef05cbb4d86df9af703c8350e90ba28d87a
tracklist19 <162835686+tracklist19@users.noreply.github.com>
2024-03-26 13:49:58 +0100

    Update fliessgewaesser_3_be.sql

--- fliessgewaesser_3_be.sql
+++ fliessgewaesser_3_be.sql
@@ -2,7 +2,6 @@
 
 CREATE VIEW  views.v_fliessgewaesser_3_be  AS
 
-
 -- aus: q_fliessgewaesser_3_be__gemNAM 
 -- ohne Vorgabe: analog zu q_fliessgewaesser_1_be, plus ax_hafenbecken, 
 -- um weniger Lücken in den Fliessgewaessern zu haben (Hafen_BE (ohne Vorgabe) extra (-> ax_hafen)) 
@@ -16,13 +15,13 @@ row_number() over(order by ax_fliessgewaesser.ogc_fid, ax_hafenbecken.ogc_fid ) 
 	fliesshafen_union.gml_id AS "OBJID", 
 	
 	ax_fliessgewaesser.hydrologischesmerkmal AS "HYD", 
-	ax_fliessgewaesser.funktion AS "FKT", 								-- aktuell nur Kanal: 8300 
+	ax_fliessgewaesser.funktion AS "FKT", 							-- aktuell nur Kanal: 8300 
 	ax_fliessgewaesser.zustand AS "ZUS", 
 	
 	hafenkanalwasser_union.name AS "NAM", 
 	
 	ax_hafenbecken.nutzung AS "NTZ", 
-	--ax_hafenbecken.seekennzahl AS "SKZ", 								-- ab ATKIS-OK 7.1.0
+	--ax_hafenbecken.seekennzahl AS "SKZ", 							-- ab ATKIS-OK 7.1.0
 
 	ax_wasserlauf.ogc_fid AS "FID_44002_Z", 
 	ax_kanal.ogc_fid AS "FID_44003_Z", 
@@ -46,7 +45,7 @@ FROM
 			ax_wasserlauf.schifffahrtskategorie, 
 			ax_wasserlauf.widmung, 
 			ax_wasserlauf.zweitname 
-		--	ax_wasserlauf.regionalsprache, 								-- ab ATKIS-OK 7.1.0
+		--	ax_wasserlauf.regionalsprache, 						-- ab ATKIS-OK 7.1.0
 	FROM ax_wasserlauf 
 
 		UNION ALL 
@@ -58,7 +57,7 @@ FROM
 			ax_kanal.schifffahrtskategorie, 
 			ax_kanal.widmung, 
 			ax_kanal.zweitname 
-		--	ax_kanal.regionalsprache, 									-- ab ATKIS-OK 7.1.0
+		--	ax_kanal.regionalsprache, 						-- ab ATKIS-OK 7.1.0
 	FROM ax_kanal 
 
 ) AS kanalWasserlauf_union 
@@ -69,8 +68,8 @@ LEFT JOIN ax_kanal ON kanalWasserlauf_union.gml_id = ax_kanal.gml_id
 RIGHT JOIN ( 
 		SELECT 
 		ax_fliessgewaesser.gml_id, 
-		ax_fliessgewaesser.advstandardmodell, 							-- fuer WHERE-Clause am Ende 
-		ax_fliessgewaesser.istteilvon,  								-- fuer JOIN mit kanalWasserlauf_union 
+		ax_fliessgewaesser.advstandardmodell, 						-- fuer WHERE-Clause am Ende 
+		ax_fliessgewaesser.istteilvon,  						-- fuer JOIN mit kanalWasserlauf_union 
 		ax_fliessgewaesser.wkb_geometry
 		FROM ax_fliessgewaesser 
 	UNION ALL 
@@ -89,7 +88,7 @@ LEFT JOIN ax_hafenbecken ON fliesshafen_union.gml_id = ax_hafenbecken.gml_id
 -- gemeinsame NamensSpalte 
 LEFT JOIN ( 
 		SELECT 
-			ax_wasserlauf.gml_id, 										-- gml_id-Spalten fuer JOIN 
+			ax_wasserlauf.gml_id, 							-- gml_id-Spalten fuer JOIN 
 			ax_wasserlauf.name 
 		FROM ax_wasserlauf 
 	UNION ALL 
@@ -103,11 +102,10 @@ LEFT JOIN (
 			ax_hafenbecken.unverschluesselt --AS name
 		FROM ax_hafenbecken 
 ) AS hafenkanalwasser_union ON hafenkanalwasser_union.gml_id = any(fliesshafen_union.istteilvon) 
-											OR hafenkanalwasser_union.gml_id = ax_hafenbecken.gml_id 
+				OR hafenkanalwasser_union.gml_id = ax_hafenbecken.gml_id 
 
-WHERE fliesshafen_union.advstandardmodell @> ('{Basis-DLM}')			-- wird hier benötigt, denn es existiert advstandardmodell = DTK25
+WHERE fliesshafen_union.advstandardmodell @> ('{Basis-DLM}')				-- wird hier benötigt, denn es existiert advstandardmodell = DTK25
 ; 
-
 
 
 ALTER TABLE views.v_fliessgewaesser_3_be OWNER TO postgres;
