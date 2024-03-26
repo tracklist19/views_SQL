fc535e613b732a341c3f374e79d0e876e51f090e
tracklist19 <162835686+tracklist19@users.noreply.github.com>
2024-03-26 14:42:57 +0100

    Update gemeindestrasse_bruecke_be.sql

--- gemeindestrasse_bruecke_be.sql
+++ gemeindestrasse_bruecke_be.sql
@@ -21,13 +21,13 @@ row_number() over(order by ax_fahrbahnachse.ogc_fid, ax_strassenachse.ogc_fid) A
 	fahrstrasse_union.anzahlDerFahrstreifen AS "FSZ", 
 	fahrstrasse_union.zustand AS "ZUS", 
 	fahrstrasse_union.oberflaechenmaterial AS "OFM", 
---	fahrstrasse_union.fahrtrichtung AS "FAR", 							-- ab ATKIS-OK 7.1.0 
+--	fahrstrasse_union.fahrtrichtung AS "FAR", 					-- ab ATKIS-OK 7.1.0 
 
 -- JOIN:   AX_Strassenachse 			
---	besondereVerkehrsbedeutung	AS "BVB", 								-- ab ATKIS-OK 7.1.0 
+--	besondereVerkehrsbedeutung	AS "BVB", 					-- ab ATKIS-OK 7.1.0 
 	breiteDesVerkehrsweges AS "BRV", 
-	verkehrsbedeutungInneroertlich AS "BDI", 							-- bis ATKIS-OK 6.0.1 
-	verkehrsbedeutungUeberoertlich AS "BDU", 							-- bis ATKIS-OK 6.0.1 
+	verkehrsbedeutungInneroertlich AS "BDI", 					-- bis ATKIS-OK 6.0.1 
+	verkehrsbedeutungUeberoertlich AS "BDU", 					-- bis ATKIS-OK 6.0.1 
 
 -- JOIN:   AX_Strasse 				
 	ax_strasse.ogc_fid AS "FID_42002_Z", 
@@ -39,7 +39,7 @@ row_number() over(order by ax_fahrbahnachse.ogc_fid, ax_strassenachse.ogc_fid) A
 	widmung AS "WDM", 
 	strassenschluessel AS "STS", 
 	zweitname AS "ZNM", 
---	regionalsprache	AS "RGS", 											-- ab ATKIS-OK 7.1.0 
+--	regionalsprache	AS "RGS", 							-- ab ATKIS-OK 7.1.0 
 
 	fahrstrasse_union.wkb_geometry AS "GEOM", 
 	
@@ -52,41 +52,41 @@ row_number() over(order by ax_fahrbahnachse.ogc_fid, ax_strassenachse.ogc_fid) A
 	tunnel_join.zustand AS "ZUS_53001", 
 	tunnel_join.durchfahrtshoehe AS "DHU", 
 	tunnel_join.breitedesobjekts AS "BRO"
---	, tunnel_join.objekthoehe AS "HHO" 									-- ab ATKIS-OK 7.1.0 
+--	, tunnel_join.objekthoehe AS "HHO" 						-- ab ATKIS-OK 7.1.0 
 
 FROM ( 
 		SELECT 
-			gml_id, 													-- fuer JOINs 
-			advstandardmodell, 											-- fuer WHERE-CLause 
+			gml_id, 							-- fuer JOINs 
+			advstandardmodell, 						-- fuer WHERE-CLause 
 			besondereFahrstreifen, 
 			breiteDerFahrbahn, 
 			funktion, 
 			anzahlDerFahrstreifen, 
 			zustand, 
 			oberflaechenmaterial, 
---			fahrtrichtung,												-- ab ATKIS-OK 7.1.0 
-			istteilvon, 												-- fuer JOIN mit ZUSO ax_strasse 
+--			fahrtrichtung,							-- ab ATKIS-OK 7.1.0 
+			istteilvon, 							-- fuer JOIN mit ZUSO ax_strasse 
 			hatdirektunten, 
 			wkb_geometry 
 		FROM ax_fahrbahnachse 
 	UNION ALL 
 		SELECT 
-			gml_id, 													-- fuer JOINs 
-			advstandardmodell, 											-- fuer WHERE-CLause 
+			gml_id, 							-- fuer JOINs 
+			advstandardmodell, 						-- fuer WHERE-CLause 
 			besondereFahrstreifen, 
 			breiteDerFahrbahn, 
 			funktion, 
 			anzahlDerFahrstreifen, 
 			zustand, 
 			oberflaechenmaterial, 
---			fahrtrichtung,												-- ab ATKIS-OK 7.1.0 
-			istteilvon, 												-- fuer JOIN mit ZUSO ax_strasse 
+--			fahrtrichtung,							-- ab ATKIS-OK 7.1.0 
+			istteilvon, 							-- fuer JOIN mit ZUSO ax_strasse 
 			hatdirektunten, 
 			wkb_geometry 
 		FROM ax_strassenachse 
 ) AS fahrstrasse_union 
 
-LEFT JOIN ax_fahrbahnachse ON ax_fahrbahnachse.gml_id = fahrstrasse_union.gml_id 					-- fuer FID-Spalte 
+LEFT JOIN ax_fahrbahnachse ON ax_fahrbahnachse.gml_id = fahrstrasse_union.gml_id 	-- fuer FID-Spalte 
 LEFT JOIN ax_strassenachse ON 	ax_strassenachse.gml_id = fahrstrasse_union.gml_id 
 
 LEFT JOIN ax_strasse ON ax_strasse.gml_id = ANY(fahrstrasse_union.istteilvon) 
@@ -98,8 +98,8 @@ RIGHT JOIN (
 		WHERE bauwerksfunktion IN (1800, 1801, 1802, 1803, 1804, 1805, 1806, 1807, 1808, 1830) 
 ) AS tunnel_join ON tunnel_join.gml_id = any(fahrstrasse_union.hatdirektunten)
 
---WHERE fahrstrasse_union.advstandardmodell @> ('{Basis-DLM}')			-- wird hier benötigt, denn es existiert advstandardmodell = DTK25 | DTK10 
-WHERE 'Basis-DLM' = ANY(fahrstrasse_union.advstandardmodell) 			-- same
+--WHERE fahrstrasse_union.advstandardmodell @> ('{Basis-DLM}')				-- wird hier benötigt, denn es existiert advstandardmodell = DTK25 | DTK10 
+WHERE 'Basis-DLM' = ANY(fahrstrasse_union.advstandardmodell) 				-- same
 	AND ax_strasse.widmung IN (1307, 9997, 9999) 
 ; 
 
